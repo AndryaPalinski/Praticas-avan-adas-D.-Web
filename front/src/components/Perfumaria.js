c96ee546a2f0cb819de3eb7abfb67e245f787fe8
Andrya :] <130005796+AndryaPalinski@users.noreply.github.com>
2024-08-20 20:51:56 -0300

    Ultimo save

--- front/src/components/Perfumaria.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../Css/stylePerfume.css'; // Importe o CSS separado para o componente
-
-function DecorativeBar() {
-  return <div className="decorative-bar"></div>;
-}
-
-export default DecorativeBar;
